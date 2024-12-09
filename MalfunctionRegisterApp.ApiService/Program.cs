acbcfc8a737968c94c4454bf94e224e66b4f08a5
michal-popek <michal.popek@interia.pl>
2024-12-10 00:03:54 +0100

    Swagger enabled on prod env

--- MalfunctionRegisterApp.ApiService/Program.cs
+++ MalfunctionRegisterApp.ApiService/Program.cs
@@ -17,11 +17,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
